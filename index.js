ff8dcfc35b9473ccd4030f4bc39df15ef8680c67
Justin Daniel <justinddaniel@gmail.com>
2018-01-18 23:41:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function setThisWithCall (arg1, arg2, arg3) {
 function setThisWithApply (arg1, arg2, arr) {
   return arg1.apply(arg2, arr)
 }
+
+function returnNewFunctionOf (fn, arg) {
+  let newFunction = returnNewFunctionOf.bind(arg)
+  return newFunction(arg);
+}
