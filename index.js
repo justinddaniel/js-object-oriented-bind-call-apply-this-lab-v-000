71c84eaab3d86dc60babe4cbbd248878bb2d6cb7
Justin Daniel <justinddaniel@gmail.com>
2018-01-18 23:37:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function justInvoke (fn) {
 function setThisWithCall (arg1, arg2, arg3) {
   return arg1.call(arg2, arg3);
 }
+
+function setThisWithApply (arg1, arg2, arr) {
+  return arg1.apply(arg2, arr)
+}
